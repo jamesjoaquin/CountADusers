2255c9271d329f2cbcb205cbd66a2e01a1c0066b
James Garside <42815264+jamesjoaquin@users.noreply.github.com>
2018-09-19 14:10:38 -0600

    Add files via upload

---
+++ UserCount.ps1
@@ -0,0 +1,16 @@
+﻿#This prompts for an AD group, outputs a count of users in the group, and prompts to run again or quit#
+
+do {
+
+
+$group = Read-Host -Prompt group
+$users = Get-ADGroupMember "$group" | Where objectclass -eq "user"
+$users.count 
+
+$response = Read-Host "Again?"
+
+}
+
+while ($response -eq "y")
+
+#james garside, 2018#
